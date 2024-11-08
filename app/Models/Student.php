80eb6269dbf1526f2bc7788b22cc48acb4a51a82
iNxTSam <inxtsam@gmail.com>
2024-11-07 20:27:31 -0500

    initial commit

---
+++ app/Models/Student.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Student extends Model
+{
+
+    protected $table = 'student';
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'lenguage'  
+    ];
+
+}
